8c69175adfcfdff85a890b51187ba9d7a0295a42
Aluno <aluno@labd.(none)>
2015-05-21 19:43:22 -0300

    Install.sh

---
+++ aula3/00-assign-iassign/ex.asm
@@ -0,0 +1,24 @@
+	SECTION .data
+
+
+NUM_OF_ROWS 	EQU 	4
+NUM_OF_COLS	EQU	3
+ARRAY_SIZE	EQU 	NUM_OF_ROWS * NUM_OF_COLS
+
+%assign 	SIZE 	2
+%iassign 	valor 	3
+
+	SECTION .text
+
+  global _start
+
+  _start:
+
+	;mov		ebx,ARRAY_SIZE
+
+	%iassign 	vAloR	4
+	mov 		ebx, vAloR
+
+	mov 		eax,1
+	int		0x80
+
---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo ln -s /home/aluno/ldm/nasm.sh /usr/bin/xnasm
+sudo ln -s /home/aluno/ldm/seeReturn.sh /usr/bin/seeReturn
+sudo ln -s /home/aluno/ldm/nasm-see.sh /usr/bin/seenasm
+
